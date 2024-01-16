fcd841d6a8d54b751b82141310b3fa4f728d8a1e
karthik <wolfenbitzyt@gmail.com>
2024-01-17 05:27:01 +0530

    oauth config setup

--- src/app.js
+++ src/app.js
@@ -3,14 +3,24 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const router = require('./routes/router.js');
 const cookieParser = require("cookie-parser");
+const passport = require('passport');
+const session = require('express-session');
+require('./passport');
 dotenv.config();
+
 
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
-app.use(cookieParser());
+app.use(session({
+    secret: process.env.secret,
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -21,7 +31,15 @@ app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
-app.use('/auth',router);
+app.get('/dashboard', (req, res) => {
+    res.send('Fashboard!');
+});
+
+app.get('/failed', (req, res) => {
+    res.send('Failed!');
+});
+
+app.use('/auth', router);
 
 
 
--- src/routes/router.js
+++ src/routes/router.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const { signup, login } = require('../controllers/auth.controller');
+const passport = require('passport');
+const { signup, login, registerWithGoogle, googleCallback } = require('../controllers/auth.controller');
 const isAuthorized = require('../middlewares/auth.middleware');
 
-router.post('/register',signup);
-router.post('/login',[isAuthorized],login);
+router.post('/register', signup);
+router.post('/login', [isAuthorized], login);
+router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+router.get('/google/callback',
+    passport.authenticate('google', {
+        failureRedirect: '/failed',
+    }),
+    function (req, res) {
+        res.redirect('/dashboard')
+
+    }
+);
 
 module.exports = router;
